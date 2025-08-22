dfc67a9a6c498c1f2f8b2b8084b09d637dd2adcc
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-08-22 21:18:28 +0530

    Query Strings Using useSearchParams Hook

--- src/Component/Admin/Admin.jsx
+++ src/Component/Admin/Admin.jsx
@@ -1,8 +1,22 @@
+import { useSearchParams } from "react-router-dom"
 
 
 const Admin = () =>{
+   const [searchParams, setsearchParams] = useSearchParams();
+   const sortBy = searchParams.get("sortBy");
+   const sortByViews = searchParams.get("sortByViews");
+   const category = searchParams.get("category");
+
+   const handleViews = () =>{
+    setsearchParams({
+        sortByViews: "views",
+        category,
+    })
+   }
     return<>
         <h2>Admin Pannel</h2>
+        <p>SortBy: {sortBy ?? sortByViews} Category: {category}</p>
+        <button onClick={handleViews}>Sort By Views</button>
     </>
 }
 
