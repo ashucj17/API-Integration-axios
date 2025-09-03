ddfb570139422d8adcecb7ffabe8094076e44021
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-09-03 16:39:33 +0530

    Enhancing user experience Loader

--- src/Component/Admin/Marketing.jsx
+++ src/Component/Admin/Marketing.jsx
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import Loader from "../Common/Loader"
 
 const Marketing = () => {
     // const [name, setName] = useState('')
@@ -14,10 +15,15 @@ const Marketing = () => {
 
     const [users,setUsers] = useState([])
 
+    const [isLoading, setIsLoading] = useState(false)
+
     useEffect(()=>{
+        setIsLoading(true)
         axios
         .get('https://jsonplaceholder.typicode.com/users')
-        .then((res)=>{setUsers(res.data)})
+        .then((res)=>{setUsers(res.data);
+        setIsLoading(false)
+        })
         .catch((error)=>{
             console.log(error)
         })
@@ -28,7 +34,7 @@ const Marketing = () => {
         <h3> Marketing Department</h3>
         <label>Enter Name: </label>
         <input onChange={(e)=>{setName(e.target.value)}} type="text" name="" id=""/>
-
+        {isLoading && <Loader />}
         {users.map((user)=> (
             <p key={user.id}>{user.name}</p>
         ))}
