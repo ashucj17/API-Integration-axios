c3d3d9c52cd9e5fe376b3470c6a193d96c17d35a
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-09-05 21:02:24 +0530

    Post request with axios

--- src/Component/Admin/Marketing.jsx
+++ src/Component/Admin/Marketing.jsx
@@ -3,7 +3,7 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 
 const Marketing = () => {
-    // const [name, setName] = useState('')
+    const [name, setName] = useState('')
     // useEffect(()=>{
     //     fetch('https://jsonplaceholder.typicode.com/users')
     //     .then((res)=>res.json())
@@ -48,10 +48,25 @@ const Marketing = () => {
         }
     }
 
+    const addUser = () =>{
+        const newUser = {
+            name,  // When key and value are same we need to right it only once.
+            id: users.length + 1
+        }
+        setUsers([newUser, ...users])
+        axios.post('https://jsonplaceholder.typicode.com/users', newUser).then((res)=>{
+            setUsers([res.data, ...users])
+        }).catch((err)=>{
+            setErrors(err.message)
+            setUsers(users)
+        })
+    }
+
     return <>
         <h3> Marketing Department</h3>
         <label>Enter Name: </label>
         <input onChange={(e)=>{setName(e.target.value)}} type="text" name="" id=""/>
+        <button onClick={addUser}>Add Data</button>
         {isLoading && <Loader />}
         {errors && <em>{errors}</em>}
         {users.map((user)=> (
